8852547fa183a9abe0ce85d09e798af74b9a5578
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-12-18 11:28:35 +0800

    Feat: shifted from my own pagination and table to the Ant Design pagination and table (finally I started understand a bit how it works

--- src/App.js
+++ src/App.js
@@ -7,12 +7,36 @@ import Navbar from "./components/Navbar";
 import Pagination from "./components/Pagination";
 import { paginate } from "./utilities/paginate";
 
+import { Table} from 'antd';
+
+
 function App() {
   const api_url = "https://api.github.com/users/mosh-hamedani/followers";
+  const { Header, Footer, Content } = Layout;
   const [followers, setFollowers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+
   const pageSize = 6;
-  const { Header, Footer, Content } = Layout;
+  const users = paginate(followers, currentPage, pageSize);
+  const columns = [
+    {
+      title: 'Avatar',
+      dataIndex: 'avatar_url',
+      key: 'avatar_url',
+      render: dataIndex => <img src={dataIndex} alt='s'/>
+    },
+    {
+      title: 'Username',
+      dataIndex: 'login',
+      key: 'login',
+    },
+    {
+      title: 'Repository',
+      dataIndex: 'html_url',
+      key: 'html_url',
+      render: dataIndex => <a href={dataIndex}>Visit</a>,
+    },
+  ];
 
   useEffect(() => {
     axios
@@ -28,7 +52,6 @@ function App() {
     setCurrentPage(page);
   }
 
-  const users = paginate(followers, currentPage, pageSize);
 
   return (
     <>
@@ -37,39 +60,13 @@ function App() {
           <Navbar />
         </Header>
         <Content>
-          <table>
-            <thead className="columns">
-              <tr>
-                <th>Avatar</th>
-                <th>Username</th>
-                <th>Repository</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((follower) => (
-                <tr key={follower.id}>
-                  <td>
-                    <img
-                      className="avatar"
-                      src={follower.avatar_url}
-                      alt={follower.login}
-                    />
-                  </td>
-                  <td>{follower.login}</td>
-                  <td>
-                    <a href={follower.html_url}>Visit</a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-          <Pagination
+        <Table dataSource={followers} pagination={{ pageSize }} columns={columns} />;
+          {/* <Pagination
             itemsCount={followers.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={handlePageChange}
-          />
+          /> */}
         </Content>
         <Footer></Footer>
       </Layout>
@@ -78,3 +75,48 @@ function App() {
 }
 
 export default App;
+
+
+// backup table
+
+// <Layout>
+// <Header>
+//   <Navbar />
+// </Header>
+// <Content>
+//   <table>
+//     <thead className="columns">
+//       <tr>
+//         <th>Avatar</th>
+//         <th>Username</th>
+//         <th>Repository</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {users.map((follower) => (
+//         <tr key={follower.id}>
+//           <td>
+//             <img
+//               className="avatar"
+//               src={follower.avatar_url}
+//               alt={follower.login}
+//             />
+//           </td>
+//           <td>{follower.login}</td>
+//           <td>
+//             <a href={follower.html_url}>Visit</a>
+//           </td>
+//         </tr>
+//       ))}
+//     </tbody>
+//   </table>
+
+//   <Pagination
+//     itemsCount={followers.length}
+//     pageSize={pageSize}
+//     currentPage={currentPage}
+//     onPageChange={handlePageChange}
+//   />
+// </Content>
+// <Footer></Footer>
+// </Layout>
