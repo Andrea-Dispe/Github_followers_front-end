220185b891a76681a1d91054fc2ad19ec71be26d
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-12-24 09:34:56 +0800

    Fix: moving the server on another repo to be served on heroku

--- server/routers.js
+++
@@ -1,8 +0,0 @@
-var router = require('express').Router();
-const controllers = require('./controllers/GithubFollowers.js');
-
-
-router.get('/', controllers.getGithubFollowers);
-
-
-module.exports = router;
